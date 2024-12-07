2b59e96f7a61a063432d1b049954d44dc055c0c6
X07U <36509249+X07U@users.noreply.github.com>
2024-12-07 16:07:36 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,35 +8,25 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-      };
+    };
 
     textfox = {
       url = "github:adriankarlen/textfox";
       inputs.nixpkgs.follows = "nixpkgs";
-      };
+    };
    
     stylix.url = "github:danth/stylix/5ab1207b2fdeb5a022f2dd7cccf6be760f1b150f";
+
   };
 
-  outputs = { self, nixpkgs, home-manager, textfox, stylix, ... } @ inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    # Default NixOS configuration
+  outputs = { self, nixpkgs, home-manager, textfox, stylix, ... } @ inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      # Pass inputs as special arguments for module access
-      specialArgs = { inherit inputs system; };
+      specialArgs = { inherit inputs; };
 
-      # Modules to import
       modules = [
         ./configuration.nix
       	stylix.nixosModules.stylix
-        home-manager.nixosModules.home-manager {
-          home-manager.extraSpecialArgs = {
-            inherit inputs;
-          };
-        }
+        inputs.home-manager.nixosModules.default
       ];
     };
   };
